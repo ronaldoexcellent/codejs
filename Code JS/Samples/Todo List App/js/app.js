37c5527abb68478affe7c1dc7201178bf14baa22
Ronaldo Excellent <120473458+ronaldoexcellent@users.noreply.github.com>
2023-07-06 19:41:47 +0100

    Delete Code JS directory

--- Code JS/Samples/Todo List App/js/app.js
+++
@@ -1,9 +0,0 @@
-// Check your console, if you see errors after connection successful, know they come from whatever code you write here.
-
-// Test Connection JavaScript Compatibility [Remove if you no longer need]...
-
-/* Try running these
-    alert($('layout').parentElement.nodeName);
-    alert(`Welcome To ${app.name}. Created by ${app.author}. Copyright ${app.copyright}.`);
-    alert(d.fullDate());
-*/
